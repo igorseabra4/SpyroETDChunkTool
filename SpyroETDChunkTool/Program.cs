b041deefadbbbd166f48109ecccee7990f0aa778
igorseabra4 <igorseabra@gmail.com>
2018-12-13 22:48:02 -0300

    Initial commit

---
+++ SpyroETDChunkTool/Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpyroETDExtractor
+{
+    public static class Extensions
+    {
+        public static bool isPS2 = false;
+
+        public static int Switch(this int a)
+        {
+            if (isPS2) return a;
+            return BitConverter.ToInt32(BitConverter.GetBytes(a).Reverse().ToArray(), 0);
+        }
+        public static uint Switch(this uint a)
+        {
+            if (isPS2) return a;
+            return BitConverter.ToUInt32(BitConverter.GetBytes(a).Reverse().ToArray(), 0);
+        }
+        public static short Switch(this short a)
+        {
+            if (isPS2) return a;
+            return BitConverter.ToInt16(BitConverter.GetBytes(a).Reverse().ToArray(), 0);
+        }
+        public static ushort Switch(this ushort a)
+        {
+            if (isPS2) return a;
+            return BitConverter.ToUInt16(BitConverter.GetBytes(a).Reverse().ToArray(), 0);
+        }
+        public static float Switch(this float a)
+        {
+            if (isPS2) return a;
+            return BitConverter.ToSingle(BitConverter.GetBytes(a).Reverse().ToArray(), 0);
+        }
+        public static byte Switch(this byte a)
+        {
+            return a;
+        }
+    }
+
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SpyroETDChunkTool v0.1 by igorseabra4");
+            Console.WriteLine();
+            Console.WriteLine("Usage:");
+            Console.WriteLine("- Drag CNK files from Spyro: Enter the Dragonfly into the executable to unpack them (GameCube files only)");
+            Console.WriteLine("- Otherwise, run the program and you'll be able to choose what to do:");
+            Console.WriteLine("- Unpacking extracts all files to a new folder in the same folder of the source CNK file.");
+            Console.WriteLine("- Packing rebuilds the archive from all files in a folder, the original archive is not required for this");
+            Console.WriteLine("- Make sure correct console is set before unpacking or packing a file from the corresponding version of the game");
+            Console.WriteLine();
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Current console mode: " + (Extensions.isPS2 ? "Playstation 2" : "GameCube"));
+
+                foreach (string s in args)
+                {
+                    Extract(s, out string outputFolder);
+                    Console.WriteLine("Done extracting " + Path.GetFileName(s) + " to " + outputFolder);
+                }
+            }
+            else
+            {
+                ConsoleKey option = ConsoleKey.D4;
+
+                while (option != ConsoleKey.D0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Current console mode: " + (Extensions.isPS2 ? "Playstation 2" : "GameCube"));
+                    Console.WriteLine("What do you want to do?");
+                    Console.WriteLine("0 = exit");
+                    Console.WriteLine("1 = unpack CNK file");
+                    Console.WriteLine("2 = pack CNK file from folder");
+                    Console.WriteLine("3 = switch console mode to " + (Extensions.isPS2 ? "GameCube" : "Playstation 2"));
+
+                    option = Console.ReadKey().Key;
+                    Console.WriteLine();
+
+                    switch (option)
+                    {
+                        case ConsoleKey.D1:
+                            OpenFileDialog openFile = new OpenFileDialog()
+                            {
+                                Filter = "CNK Files|*.cnk",
+                                Multiselect = true
+                            };
+                            if (openFile.ShowDialog(new Form() { TopMost = true }) == DialogResult.OK)
+                            {
+                                foreach (string s in openFile.FileNames)
+                                {
+                                    Extract(s, out string outputFolder);
+                                    Console.WriteLine("Done extracting " + Path.GetFileName(s) + " to " + outputFolder);
+                                }
+                            }
+                            else
+                                Console.WriteLine("Operation cancelled.");
+                            break;
+                        case ConsoleKey.D2:
+                            FolderBrowserDialog openFolder = new FolderBrowserDialog();
+                            SaveFileDialog saveFile = new SaveFileDialog()
+                            {
+                                Filter = "CNK Files|*.cnk"
+                            };
+
+                            if (openFolder.ShowDialog(new Form() { TopMost = true }) == DialogResult.OK)
+                            {
+                                if (saveFile.ShowDialog(new Form() { TopMost = true }) == DialogResult.OK)
+                                {
+                                    Create(openFolder.SelectedPath, saveFile.FileName);
+                                    Console.WriteLine("Done creating " + Path.GetFileName(saveFile.FileName) + " from " + Path.GetFileName(openFolder.SelectedPath));
+                                }
+                                else
+                                    Console.WriteLine("Operation cancelled.");
+                            }
+                            else
+                                Console.WriteLine("Operation cancelled.");
+                            break;
+                        case ConsoleKey.D3:
+                            Extensions.isPS2 = !Extensions.isPS2;
+                            break;
+                    }
+                }
+            }
+        }
+
+        public class FileEntry
+        {
+            public uint Hash;
+            public uint Offset;
+            public uint Size;
+            public uint Null;
+
+            public byte[] Data;
+        }
+
+        private static void Extract(string s, out string outputFolder)
+        {
+            BinaryReader binaryReader = new BinaryReader(new FileStream(s, FileMode.Open));
+            binaryReader.BaseStream.Position = 0xC;
+
+            int fileCount = binaryReader.ReadInt32().Switch();
+
+            List<FileEntry> fileEntries = new List<FileEntry>();
+
+            for (int i = 0; i < fileCount; i++)
+            {
+                fileEntries.Add(new FileEntry
+                {
+                    Hash = binaryReader.ReadUInt32().Switch(),
+                    Offset = binaryReader.ReadUInt32().Switch(),
+                    Size = binaryReader.ReadUInt32().Switch(),
+                    Null = binaryReader.ReadUInt32().Switch()
+                });
+            }
+
+            outputFolder = Path.GetDirectoryName(s) + "\\" + Path.GetFileNameWithoutExtension(s) + "_out";
+            Directory.CreateDirectory(outputFolder);
+
+            foreach (FileEntry file in fileEntries)
+            {
+                binaryReader.BaseStream.Position = file.Offset;
+                file.Data = binaryReader.ReadBytes((int)file.Size);
+                File.WriteAllBytes(Path.Combine(outputFolder, file.Hash.ToString("X8")), file.Data);
+            }
+        }
+
+        private static void Create(string folderPath, string destinationCNK)
+        {
+            string[] files = Directory.GetFiles(folderPath);
+
+            List<FileEntry> fileEntries = new List<FileEntry>();
+            List<byte> data = new List<byte>();
+
+            foreach (string s in files)
+            {
+                byte[] Data = File.ReadAllBytes(s);
+                fileEntries.Add(new FileEntry
+                {
+                    Hash = Convert.ToUInt32(Path.GetFileName(s), 16),
+                    Data = Data,
+                    Size = (uint)Data.Length,
+                    Null = (uint)(Extensions.isPS2 ? 2 : 0x00020000)
+                });
+            }
+
+            fileEntries = fileEntries.OrderBy(f => f.Hash).ToList();
+
+            int headerLenght = (fileEntries.Count + 2) * 16;
+            while (headerLenght % 0x20 != 0 || headerLenght < 0x2000)
+                headerLenght++;
+
+            foreach (FileEntry file in fileEntries)
+            {
+                file.Offset = (uint)(data.Count + headerLenght);
+                data.AddRange(file.Data);
+                while (data.Count % 0x20 != 0)
+                    data.Add(0);
+            }
+
+            BinaryWriter binaryWriter = new BinaryWriter(new FileStream(destinationCNK, FileMode.Create));
+            binaryWriter.Write(1);
+            binaryWriter.Write(1);
+            binaryWriter.Write(0);
+            binaryWriter.Write(fileEntries.Count.Switch());
+
+            foreach (FileEntry file in fileEntries)
+            {
+                binaryWriter.Write(file.Hash.Switch());
+                binaryWriter.Write(file.Offset.Switch());
+                binaryWriter.Write(file.Size.Switch());
+                binaryWriter.Write(file.Null.Switch());
+            }
+
+            binaryWriter.BaseStream.Position = headerLenght;
+            binaryWriter.Write(data.ToArray());
+
+            binaryWriter.Close();
+        }
+    }
+}
---
+++ SpyroETDChunkTool/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("SpyroETDExtractor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpyroETDExtractor")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("a2d00d88-cd3e-47e6-a925-977fefd1d63c")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
